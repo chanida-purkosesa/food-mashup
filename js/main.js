2a8b9eeec57c396172551a72f1299a3e84ecbab2
Chanida Purkosesa <Chanida.Purkosesa@JCrew.Com>
2013-10-18 16:20:21 -0400

    Using IP Geolocation API, return user's location (city/state)

--- js/main.js
+++ js/main.js
@@ -1,1 +1,19 @@
+var FoodMashup = (function() {
+    var getLocation = function() {
+        $.getJSON('http://ip-api.com/json', function(data) {
+            console.log ('City: ' + data.city + ', State: ' + data.regionName);
+        });
+    }
+    var init = function() {
+        getLocation();
+    };
+    return {
+        init : function() {
+            init();
+        }
+    }
+})();
 
+$(document).ready(function() {
+    FoodMashup.init();
+});
