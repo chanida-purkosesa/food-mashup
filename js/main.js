4f27b1c8c2c60cdbaebd692018f2b38123aa14d2
Chanida Purkosesa <Chanida.Purkosesa@JCrew.Com>
2013-10-21 11:20:14 -0400

    Updated maps to show multiple markers

--- js/main.js
+++ js/main.js
@@ -1,12 +1,12 @@
 var FoodMashup = (function() {
-    var getUserLocation = $.getJSON('http://ip-api.com/json');
+    var locations = [];
 
     var loadTemplate = function() {
         $('body').append('<div id="resultsTpl"></div>');
         $('#resultsTpl').load('results.html');
     };
     var setUserLocation = function() {
-        getUserLocation.then(function (data) {
+        $.getJSON('http://ip-api.com/json').then(function (data) {
             var city = data.city;
             var state = data.region;
             $('#searchLocation').val(city + ', ' + state);
@@ -67,26 +67,65 @@ var FoodMashup = (function() {
             dataType: 'jsonp',
             jsonpCallback : 'yelpCallback',
             success: function(data) {
+                console.log (data);
+                locations = [];
+                getGeocode(data);
+                $('#searchResultsMap').show();
+                $.getScript('http://maps.googleapis.com/maps/api/js?key=AIzaSyBwu1ysynoW9TdftIqqo8gtcmFcEAuqtCY&sensor=false&callback=initGoogleMap');
+
                 var template = _.template($('#resultsTemplate').html(), {'resultSet': data});
                 $('#searchResult').html(template);
-                console.log (data);
-                $('#searchResultsMap').show();
-                $.getScript('http://maps.googleapis.com/maps/api/js?key=AIzaSyBwu1ysynoW9TdftIqqo8gtcmFcEAuqtCY&sensor=false&callback=getGoogleMap');
             }
         });
     };
-    getGoogleMap = function() {
-        getUserLocation.then(function (data) {
-            var lat = data.lat;
-            var lon = data.lon;
-            
-            var mapOptions = {
-                center: new google.maps.LatLng(lat, lon),
-                zoom: 15,
-                mapTypeId: google.maps.MapTypeId.ROADMAP
-            };
-            var map = new google.maps.Map(document.getElementById("mapCanvas"), mapOptions);
+    getGeocode = function(data) {
+        var place;
+
+        $.each (data.businesses, function(index, value) {
+            var name = data.businesses[index].name;
+            var street = data.businesses[index].location.address;
+            var city = data.businesses[index].location.city;
+            var state = data.businesses[index].location.state_code;
+            var address = street + ',' + city + ',' + state;
+            address = address.replace(/ /g, '+');
+
+            $.ajax({
+                url: 'http://maps.googleapis.com/maps/api/geocode/json?address=' + address + '&sensor=false',
+                dataType: 'json',
+                success: function(dataCoords) {
+                    var lat = dataCoords.results[0].geometry.location.lat;
+                    var lng = dataCoords.results[0].geometry.location.lng;
+                    place = [name, lat, lng, ++index];
+                    locations.push(place);
+                }
+            });
         });
+    };
+    initGoogleMap = function() {
+        var mapOptions = {
+            zoom: 15,
+            mapTypeId: google.maps.MapTypeId.ROADMAP
+        };
+        var map = new google.maps.Map(document.getElementById("mapCanvas"), mapOptions);
+        setGoogleMarkers(map, locations);
+    };
+    setGoogleMarkers = function(map, locations) {
+        var bounds = new google.maps.LatLngBounds();
+
+        $.each(locations, function(index, value) {
+            var place = locations[index];
+            var placeCoords = new google.maps.LatLng(place[1], place[2]);
+
+            var marker = new google.maps.Marker({
+                position: placeCoords,
+                map: map,
+                title: place[0],
+                zIndex: place[3]
+            });
+
+            bounds.extend(marker.position);
+        });
+        map.fitBounds(bounds);
     };
     var init = function() {
         setUserLocation();
